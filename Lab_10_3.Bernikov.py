5b4a9f9e1f16ea93e09779a3601ddaa9a6d13e7a
Bandos21 <56268924+Bandos21@users.noreply.github.com>
2019-12-17 17:33:42 +0200

    Add files via upload

---
+++ Lab_10_3.Bernikov.py
@@ -0,0 +1,16 @@
+import numpy as np
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
+t=np.arange(0,20,0.01)
+def padenie_func():
+    
+    
+    
+    
+    
+    
+    
+G=6.67*10**(-11) 
+M=5.97*10**24
+r=6371
+v0=   
