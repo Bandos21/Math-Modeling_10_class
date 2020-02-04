1cd55fcb08a77899ea3571753e23cff9903fcaf1
Bandos21 <56268924+Bandos21@users.noreply.github.com>
2020-02-04 17:56:32 +0200

    Add files via upload

--- untitled3.py
+++ untitled3.py
@@ -3,108 +3,120 @@ from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 from matplotlib.animation import ArtistAnimation
 
-seconds_in_year= 365*24*60*60
-seconds_in_day= 24*60*60
-years=1
-t=np.arange(0,years*seconds_in_year,seconds_in_day)
+t = np.arange(10**(-7),1.3*10**(-7), 10**(-9))
 
-def move_func(s,t):
+def move_func(s, t):
     (x1, v_x1, y1, v_y1,
      x2, v_x2, y2, v_y2,
      x3, v_x3, y3, v_y3,
-     x4, v_x4, y4, v_y4)= s
-     
+     x4, v_x4, y4, v_y4) = s
+
+ 
     dxdt1=v_x1
-    dv_xdt1=(-G*m2*(x1-x2)/((x1-x2)**2+(y1-y2)**2)**1.5
-             -G*m3*(x1-x3)/((x1-x3)**2+(y1-y3)**2)**1.5)
+    dv_xdt1=( k * q1 * q2 / m1 * (x1 - x2) / ((x1 - x2)**2 + (y1 - y2)**2)**1.5
+             + k * q1 * q3 / m1 * (x1 - x3) / ((x1 - x3)**2 + (y1 - y3)**2)**1.5
+             + k * q1 * q4 / m1 * (x1 - x4) / ((x1 - x4)**2 + (y1 - y4)**2)**1.5)
      
     dydt1=v_y1
-    dv_ydt1=(-G*m2*(y1-y2)/((x1-x2)**2+(y1-y2)**2)**1.5
-             -G*m3*(y1-y3)/((x1-x3)**2+(y1-y3)**2)**1.5)
+    dv_ydt1=(k * q1 * q2 / m1 * (y1 - y2) / ((x1 - x2)**2 + (y1 -y2)**2)**1.5
+             + k * q1 * q3 / m1 * (y1 - y3) / ((x1 - x3)**2 + (y1 - y3)**2)**1.5
+             + k * q1 * q4 / m1 * (y1 - y4) / ((x1 - x4)**2 + (y1 - y4)**2)**1.5)
      
     dxdt2=v_x2
-    dv_xdt2=(-G*m1*(x2-x1)/((x2-x1)**2+(y2-y1)**2)**1.5
-             -G*m3*(x2-x3)/((x2-x3)**2+(y2-y3)**2)**1.5)
+    dv_xdt2=(k * q2 * q1 / m2 * (x2 - x1) / ((x2 - x1)**2 + (y2 -y1)**2)**1.5
+             + k * q2 * q3 / m2 * (x2 - x3) / ((x2 - x3)**2 + (y2 - y3)**2)**1.5
+             + k * q2 * q4 / m2 * (x2 - x4) / ((x2 - x4)**2 + (y2 - y4)**2)**1.5)
      
     dydt2=v_y2
-    dv_ydt2=(-G*m1*(y2-y1)/((x2-x1)**2+(y2-y1)**2)**1.5
-            -G*m3*(y2-y3)/((x2-x3)**2+(y2-y3)**2)**1.5)
+    dv_ydt2=(k * q2 * q1 / m2 * (y2 - y1) / ((x2 - x1)**2 + (y2 -y1)**2)**1.5
+             + k * q2 * q3 / m2 * (y2 - y3) / ((x2 - x3)**2 + (y2 - y3)**2)**1.5
+             + k * q2 * q4 / m2 * (y2 - y4) / ((x2 - x4)**2 + (y2 - y4)**2)**1.5)
      
     dxdt3=v_x3
-    dv_xdt3=(-G*m1*(x3-x1)/((x3-x1)**2+(y3-y1)**2)**1.5
-             -G*m2*(x3-x2)/((x3-x2)**2+(y3-y2)**2)**1.5)
+    dv_xdt3=( k * q3 * q1 / m3 * (x3 - x1) / ((x3 - x1)**2 + (y3 -y1)**2)**1.5
+             + k * q3 * q2 / m3 * (x3 - x2) / ((x3 - x2)**2 + (y3 - y2)**2)**1.5
+             + k * q3 * q4 / m3 * (x3 - x4) / ((x3 - x4)**2 + (y3 - y4)**2)**1.5)
      
     dydt3=v_y3
-    dv_ydt3=(-G*m1*(y3-y1)/((x3-x1)**2+(y3-y1)**2)**1.5
-             -G*m2*(y3-y2)/((x3-x2)**2+(y3-y2)**2)**1.5)
+    dv_ydt3=(k * q3 * q1 / m3 * (y3 - y1) / ((x3 - x1)**2 + (y3 -y1)**2)**1.5
+             + k * q3 * q2 / m3 * (y3 - y2) / ((x3 - x2)**2 + (y3 - y2)**2)**1.5
+             + k * q3 * q4 / m3 * (y3 - y4) / ((x3 - x4)**2 + (y3 - y4)**2)**1.5)
     
     dxdt4=v_x4
-    dv_xdt4=(-G*m1*(x4-x1)/((x4-x1)**2+(y4-y1)**2)**1.5)
+    dv_xdt4=( k * q4 * q1 / m4 * (x4 - x1) / ((x4 - x1)**2 + (y4 -y1)**2)**1.5
+             + k * q4 * q2 / m4 * (x4 - x2) / ((x4 - x2)**2 + (y4 - y2)**2)**1.5
+             + k * q4 * q3 / m4 * (x4- x3) / ((x4 - x3)**2 + (y4 - y3)**2)**1.5)
      
     dydt4=v_y4
-    dv_ydt4=(-G*m1*(y4-y1)/((x4-x1)**2+(y4-y1)**2)**1.5)
-             
+    dv_ydt4=( k * q4 * q1 / m4 * (y4 - y1) / ((x4 - x1)**2 + (y4 - y1)**2)**1.5
+             + k * q4 * q2 / m4 * (y4 - y2) / ((x4 - x2)**2 + (y4 - y2)**2)**1.5
+             + k * q4 * q3 / m4 * (y4 - y3) / ((x4 - x3)**2 + (y4 - y3)**2)**1.5)
+    
+    
+    
      
     return(dxdt1, dv_xdt1, dydt1, dv_ydt1,
-           dxdt2, dv_xdt2, dydt2, dv_ydt2,
-           dxdt3, dv_xdt3, dydt3, dv_ydt3,
-           dxdt4, dv_xdt4, dydt4, dv_ydt4)
-
-ae = 149.6 * 10**9
-Msun = 1.98847 * 10**30
-G = 6.67 * 10**(-11)
-m1 = 1.06 * Msun
-m2 = 0.6 * Msun
-m3 = 0.3 * Msun
-m4 = 0.3 * Msun
-Mall = m1 + m2 + m3 + m4
-
-x10=6.15*10**9
-v_x10=(G * Mall / (5.65 * ae))**0.5
-y10=5.65 * ae
-v_y10=0
-
-x20=0
-v_x20=(G * Mall / (4.65 * ae))**0.5
-y20=-4.65 * ae
-v_y20=0
-
-x30=0
-v_x30=(G * Mall / (8.65 * ae))**0.5
-y30=-8.65 * ae
-v_y30=0
-
-x40=6.1*10**9
-v_x40=(G * Mall / (5.5 * ae))**0.5
-y40=5.5 * ae
-v_y40=0
-
-s0= ( x10, v_x10, y10, v_y10,    
-      x20, v_x20, y20, v_y20,
-      x30, v_x30, y30, v_y30,
-      x40, v_x40, y40, v_y40)
-
-
-
-sol= odeint(move_func, s0, t)
-fig= plt.figure()
-bodys= []
-
-for i in range (0, len(t), 1):
-    body1, = plt.plot(sol[:i, 0], sol[:i,2], '-', color='r')
-    body1_line, = plt.plot(sol[i, 0], sol[i,2], 'o', color='r')
-#    
-    body2, = plt.plot(sol[:i, 4], sol[:i,6], '-', color='g')
-    body2_line, = plt.plot(sol[i, 4], sol[i,6], 'o', color='g')
+            dxdt2, dv_xdt2, dydt2, dv_ydt2,
+            dxdt3, dv_xdt3, dydt3, dv_ydt3,
+            dxdt4, dv_xdt4, dydt4, dv_ydt4)
     
-    body3, = plt.plot(sol[:i, 8], sol[:i,10], '-', color='b')
-    body3_line, = plt.plot(sol[i, 8], sol[i,10], 'o', color='b')
+G=0
+a=0.008
+v=4*10**6
+x10 = -a
+v_x10 = -v*np.sin(np.pi/4)
+y10 = a
+v_y10 = -v*np.cos(np.pi/4)
+
+x20 = -a
+v_x20 = v*np.cos(np.pi/4)
+y20 = -a
+v_y20 = -v*np.sin(np.pi/4)
+
+x30 = a
+v_x30 = v*np.sin(np.pi/4)
+y30 = -a
+v_y30 = v*np.cos(np.pi/4)
+
+x40 = a
+v_x40 = -v*np.cos(np.pi/4)
+y40 = a
+v_y40 = v*np.sin(np.pi/4)
+     
+s0 = (x10, v_x10,y10,v_y10,
+      x20, v_x20,y20,v_y20,
+      x30, v_x30,y30,v_y30,
+      x40, v_x40,y40,v_y40) 
+   
+m1 = 1.1*10**(-27)
+q1 = 1.1*10**(-13)
+m2 = 1.1*10**(-27)
+q2 = -1.1*10**(-13)
+m3 = 1.1*10**(-27)
+q3 = 1.1*10**(-13)
+m4 = 1.1*10**(-27)
+q4 = -1.1*10**(-13)
+k = 8.98755 * 10**9
+
+sol = odeint(move_func, s0, t)
+
+fig = plt.figure()
+bodys = []
+for i in range(0, len(t), 1):
+    body1, = plt.plot(sol[:i,0], sol[:i,2],'-',color='r')
+    body1_line, = plt.plot(sol[:i,0], sol[:i,2],'-',color='r')
     
-    body4, = plt.plot(sol[:i, 4], sol[:i,2], '-', color='y')
-    body4_line, = plt.plot(sol[i, 4], sol[i,2], 'o', color='y')
+    body2, = plt.plot(sol[:i,4], sol[:i,6],'-',color='g')
+    body2_line, = plt.plot(sol[:i,4], sol[:i,6],'-',color='g')
+    
+    body3, = plt.plot(sol[:i,8], sol[:i,10],'-',color='b')
+    body3_line, = plt.plot(sol[:i,8], sol[:i,10],'-',color='b')
+    
+    body4, = plt.plot(sol[:i,12], sol[:i,14],'-',color='m')
+    body4_line, = plt.plot(sol[:i,12], sol[:i,14],'-',color='m')
     
     bodys.append([body1,body1_line,body2,body2_line,body3,body3_line,body4,body4_line])
-
-ani = ArtistAnimation(fig, bodys, interval=50)
+    
+ani = ArtistAnimation(fig, bodys, interval=20)
 plt.axis('equal')
-ani.save('hisdjasf.gif')
+ani.save("3.gif")
